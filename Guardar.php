7ab05ba3fd06cd7a630e4f820049d786dec31301
Carlos Leon <carlos.leon.moran@gmail.com>
2016-07-02 07:56:16 +0000

    Funcion guardar aun no terminada faltan errores logicos

--- Guardar.php
+++ Guardar.php
@@ -1,22 +1,31 @@
 <?php
 
 include ("Pantalla.php");
+include("Preguntas.php");
 @session_start();
-$ar =$_SESSION["pinE"]."P";
-$nombre = $_SESSION["nombre"];
-$puntaje = $_SESSION["puntaje"];
-$fila= $_SESSION["fila"];
-$fp = fopen($ar, "a");
-$escribir = strtoupper($nombre).";".$puntaje.";".$fila;
-
-fwrite($fp, $escribir . PHP_EOL);
-
-$mostrar= "<ul>"
-.    "<li>".$nombre."</li>"
-.    "<li>Puntaje: ".$puntaje."</li>"
-.    "<li>Fila: ".$fila."</li>"
-        . "</ul>";
-
-$ver = new Pantalla(strtoupper($nombre),"Finalizado");
-$ver->setcuerpo($mostrar);
-$ver->mostrar();
+if($_SESSION["tipoSesion"]=="ESTUDIANTE"){
+     $ar =$_SESSION["pinE"]."P";
+     $nombre = $_SESSION["nombre"];
+     $puntaje = $_SESSION["puntaje"];
+     $fila= $_SESSION["fila"];
+     $fp = fopen($ar, "a");
+     $escribir = strtoupper($nombre).";".$puntaje.";".$fila;
+     
+     fwrite($fp, $escribir . PHP_EOL);
+     
+     $mostrar= "<ul>"
+     .    "<li>".$nombre."</li>"
+     .    "<li>Puntaje: ".$puntaje."</li>"
+     .    "<li>Fila: ".$fila."</li>"
+             . "</ul>";
+     
+     $ver = new Pantalla(strtoupper($nombre),"Finalizado");
+     $ver->setcuerpo($mostrar);
+     $ver->mostrar();
+}
+else{
+     $fp =fopen($_SESSION["pinG"],"a");
+     foreach ($_SESSION["cuestionario"] as $preg){
+          fwrite($fp, $preg . PHP_EOL);
+     }
+}
